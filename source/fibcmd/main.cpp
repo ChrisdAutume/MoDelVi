60624e03878fd8d9d2d10f4e6a03dac84bccf545
Christian d'Autume <christian@dautume.fr>
2015-05-09 12:02:32 +0100

    Treshold de la lib et des couleurs, ajout de la gestion du bruit basique

--- source/fibcmd/main.cpp
+++ source/fibcmd/main.cpp
@@ -22,30 +22,43 @@
 int main(int /*argc*/, char* /*argv*/[])
 {
 //    std::cout << "Version: " << MODELVI_VERSION << std::endl;  
-     std::string path = "../../data/samplePicture/frame";
-    std::cout << "Enter a number between 123 and 2399" << std::endl;
+    
+    std::string path = "../../data/sampleIcub/frame";
+    
+    /*std::cout << "Enter a number between 000 and 377" << std::endl;
     std::string number;
     std::cin >> number;
-
-    std::string fullpath = path + number;
+     * */
+    
+    std::string fullpath = "";
     fullpath += ".ppm";
     
-    //MoDelVi::Acquisition::FileImage* file= new MoDelVi::Acquisition::FileImage{"../data/samplePicture/frame147.ppm"};
-    MoDelVi::Acquisition::FileImage* file= new MoDelVi::Acquisition::FileImage{fullpath};
+    MoDelVi::Acquisition::FileImage* file= new MoDelVi::Acquisition::FileImage{"../data/samplePicture/frame147.ppm"};
+    //MoDelVi::Acquisition::FileImage* file= new MoDelVi::Acquisition::FileImage{fullpath};
     //MoDelVi::Acquisition::FileImage* file= new MoDelVi::Acquisition::FileImage{"frame000.ppm"};
-    //MoDelVi::Analyse::ShapesAnalyser shapes{file};
-    MoDelVi::Analyse::BlobAnalyser blob(file);
+    //MoDelVi::Analyse::ShapesAnalyser shapes(file);
+    MoDelVi::Analyse::BlobAnalyser blob;
+   
     
-    int blurr=3,fov=100, bright=200;
-    cvNamedWindow("Shapes detection");
+    
+    int blurr=3,fov=100, bright=238;
+    //cvNamedWindow("Shapes detection");
     cvNamedWindow("Control", 1);
     //cv::createTrackbar("Gray Treshold", "Control", shapes.getGrayTreshold(), 255);
     cv::createTrackbar("Blurr", "Control", &blurr, 255);
     cv::createTrackbar("FOV", "Control", &fov, 100);
     cv::createTrackbar("Brightness 0->200", "Control", &bright, 400);
-
+    int compteur = 100;
     while(true)
     {
+        compteur++;
+        if(compteur>377) compteur=100;
+        delete file;
+        std::ostringstream s;
+        s << compteur;
+        fullpath = path + s.str();
+        fullpath += ".ppm";
+        file = new MoDelVi::Acquisition::FileImage{fullpath};
         //ODD number is needed for blurr
         while(blurr%2 == 0)
             blurr++;
@@ -56,13 +69,14 @@ int main(int /*argc*/, char* /*argv*/[])
         file->setFov(fov);
         file->setBrightness(bright-200);
         file->prepareImage();
+        
         //shapes.proceed();
-        blob.proceed();
+        blob.proceed(file);
         //cvShowImage("Shapes detection", shapes.getResultIpl());
-        cv::imshow("Shapes detection", *blob.getResultMat());
+        cv::imshow("Blob detection", *blob.getResultMat());
 //while(true)
  //   {
-        if (cv::waitKey(30) >= 0)
+        if (cv::waitKey(50) >= 0)
         {
             std::cout<<"Touch pressed by user. Bye :D"<<std::endl;
             break;
